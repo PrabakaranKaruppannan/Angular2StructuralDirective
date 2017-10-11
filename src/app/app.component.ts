ed90c2f98162f351b0586062ee9fbbd97f31144e
Prabakaran, Karuppannan (GfK) <Karuppannan.Prabakaran@gfk.com>
2017-10-11 19:48:18 +0800

    Learn structural directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Structural Directive';
+  show = false;
+
+  movies=['Gaurdian Of Galaxy', 'X Men', 'Avenger'];
+  showName() {
+    this.show = true;
+  }
 }
